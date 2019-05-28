f9a928f38fa6b7ce0eeb02b403d64f7b30cde010
381706-2BandenkovD <43254290+381706-2BandenkovD@users.noreply.github.com>
2019-05-28 22:54:05 +0300

    Revert "almost done"

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,22 +1,7 @@
 #include <iostream>
-#include "TMatrix.h"
 
-
-int main(int argc, char* argv[]) 
+int main()
 {
-  setlocale(LC_ALL, "");
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
-  cout << "Testing triangular matrix support programs" << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++) 
-    {
-      a[i][j] = i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+  std::cout << "\nHello Boris!!!\n";
   return 0;
 }
--- vectorlib/main.cpp
+++ vectorlib/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-
-void it_is_not_main()
+void not_main()
 {
   std::cout << "\nHi\n";
 }
